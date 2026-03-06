960d95691fd1abd6b815178923e5e97ed7bce626
ladylobentley <bentleyle1@g.cofc.edu>
2026-03-06 14:42:42 -0500

    updated header and footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,8 +4,21 @@ function Header(props){
 
     return(
         <header className="header">
-            <div class="logo">
-            Stable.<span>AI</span>
+            <div className="headerLeft">
+                <button
+                    className='menuToggle'
+                    onClick={props.toggleNav}
+                    aria-label="Toggle navigation"
+                >
+                    <span className="material-symbols-rounded">menu</span>
+                </button>
+
+                <div className="logo">
+                    <span className="logoIcon">
+                        <img src="/stableai-icon.png" />
+                    </span>
+                    Stable.<span>AI</span>
+                </div>
             </div>
             <UserGreeting isLoggedIn={true} name={props.user}/>
         </header>
